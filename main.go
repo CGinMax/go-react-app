af09bcac6211b9e27593c73237835f3221462b4b
CGinMax <cginmax1998@gmail.com>
2021-08-12 20:59:15 +0800

    fix project

--- main.go
+++ main.go
@@ -7,12 +7,21 @@ import (
 
 func main() {
 	r := gin.Default()
+	r.Static("/static/", "./static/")
 	r.LoadHTMLGlob("static/*.html")
 	r.GET("/index", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{
 			"Title": "Go-React",
 			"Name": "Cooper Gin",
 		})
+	})
+	r.GET("/jsonData", func(c *gin.Context) {
+		data := map[string]interface{}{
+			"lang": "Gin框架",
+			"tag":  "<br>",
+		}
+
+		c.AsciiJSON(http.StatusOK, data)
 	})
 	r.Run(":19981") // listen and serve on 0.0.0.0:8080
 }
